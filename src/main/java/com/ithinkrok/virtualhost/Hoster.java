01a878087ad7a8427f8f9203ac511cc765fc33a4
Paul Durbaba <pauldurbz@gmail.com>
2015-01-20 22:30:50 +0000

    Added input listening and commands (reload command)

--- src/main/java/com/ithinkrok/virtualhost/Hoster.java
+++ src/main/java/com/ithinkrok/virtualhost/Hoster.java
@@ -5,20 +5,20 @@ import com.ithinkrok.virtualhost.io.MinecraftOutputStream;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 /**
  * Created by paul on 19/01/15.
  */
-public class Hoster extends Thread{
+public class Hoster extends Thread {
 
     private ServerSocket socket;
 
     //private ArrayList<User> users = new ArrayList<>();
 
-    private HashMap<Address, Address> virtualServers = new HashMap<>();
+    private ConcurrentHashMap<Address, Address> virtualServers = new ConcurrentHashMap<>();
 
     private byte[] defaultStatus;
 
@@ -42,21 +42,42 @@ public class Hoster extends Thread{
         Hoster host = instance = new Hoster();
 
         host.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            String line = reader.readLine().trim().toLowerCase();
+
+            switch(line){
+                case "reload":
+                    LOGGER.info("Reloading all files. No users will be disconnected");
+                    host.reload();
+                    break;
+                case "exit":
+                    LOGGER.warning("exit command issued. Stopping. All users will be disconnected");
+                    System.exit(0);
+                    break;
+                default:
+                    LOGGER.info("Unknown command.");
+            }
+        }
     }
 
     public Hoster() throws IOException {
         int port = 25565;
 
         File propertiesFile = new File("config.properties");
-        if(!propertiesFile.exists()){
+        if (!propertiesFile.exists()) {
             writeResource("config.properties", propertiesFile);
         } else {
             Properties props = new Properties();
-            props.load(new FileInputStream(propertiesFile));
+            FileInputStream in = new FileInputStream(propertiesFile);
+            props.load(in);
+            in.close();
 
-            try{
+            try {
                 port = Integer.parseInt(props.getProperty("port", "25565"));
-            } catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 LOGGER.severe("Invalid port. Exiting");
                 return;
             }
@@ -68,16 +89,16 @@ public class Hoster extends Thread{
         }
 
         File serversFile = new File("virtualservers.servers");
-        if(!serversFile.exists()){
+        if (!serversFile.exists()) {
             writeResource("virtualservers.servers", serversFile);
         }
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(serversFile)));
 
         String line;
-        while((line = reader.readLine()) != null){
+        while ((line = reader.readLine()) != null) {
             line = line.trim();
-            if(line.isEmpty()) continue;
+            if (line.isEmpty()) continue;
             String[] parts = line.split(" ");
 
             Address incoming = new Address(parts[0]);
@@ -88,19 +109,80 @@ public class Hoster extends Thread{
             LOGGER.info("Converting " + incoming.toString() + " to " + virtual.toString());
         }
 
+        reader.close();
+
         getDefaultStatus();
 
         socket = new ServerSocket(port);
     }
 
-    public Address getVirtualAddress(Address external){
+    public Address getVirtualAddress(Address external) {
         return virtualServers.get(external);
     }
 
 
+    public void reload() throws IOException {
+        int port;
+
+        File propertiesFile = new File("config.properties");
+        if (!propertiesFile.exists()) {
+            writeResource("config.properties", propertiesFile);
+        } else {
+            Properties props = new Properties();
+            FileInputStream in = new FileInputStream(propertiesFile);
+            props.load(in);
+            in.close();
+
+            try {
+                port = Integer.parseInt(props.getProperty("port", "25565"));
+            } catch (NumberFormatException e) {
+                LOGGER.warning("Failed to read port in reload.");
+                return;
+            }
+
+            if(port != socket.getLocalPort()){
+                LOGGER.warning("Port changed. Please restart for it to take effect");
+            }
+
+            defaultVersion = props.getProperty("default_version", "1.8");
+            defaultProtocol = props.getProperty("default_protocol", "5");
+            defaultDescription = props.getProperty("default_description", "{\"text\":\"Unknown virtual host\"}");
+
+        }
+
+        File serversFile = new File("virtualservers.servers");
+        if (!serversFile.exists()) {
+            writeResource("virtualservers.servers", serversFile);
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(serversFile)));
+
+        ConcurrentHashMap<Address, Address> newVirtualServers = new ConcurrentHashMap<>();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            line = line.trim();
+            if (line.isEmpty()) continue;
+            String[] parts = line.split(" ");
+
+            Address incoming = new Address(parts[0]);
+            Address virtual = new Address(parts[1]);
+
+            newVirtualServers.put(incoming, virtual);
+
+            LOGGER.info("Converting " + incoming.toString() + " to " + virtual.toString());
+        }
+
+        virtualServers = newVirtualServers;
+
+        reader.close();
+
+        generateDefaultStatus();
+    }
+
     @Override
     public void run() {
-        while(!socket.isClosed()){
+        while (!socket.isClosed()) {
             try {
                 Socket user = socket.accept();
 
@@ -119,9 +201,10 @@ public class Hoster extends Thread{
         }
     }
 
+
     private void writeResource(String resource, File file) throws IOException {
-        try(InputStream in = Hoster.class.getClassLoader().getResourceAsStream(resource);
-            FileOutputStream out = new FileOutputStream(file)) {
+        try (InputStream in = Hoster.class.getClassLoader().getResourceAsStream(resource);
+             FileOutputStream out = new FileOutputStream(file)) {
             int read;
 
             while ((read = in.read()) != -1) {
@@ -134,42 +217,22 @@ public class Hoster extends Thread{
     }
 
     public byte[] getDefaultStatus() throws IOException {
-        if(defaultStatus != null) return  defaultStatus;
+        if (defaultStatus != null) return defaultStatus;
 
+        return generateDefaultStatus();
+    }
+
+    private byte[] generateDefaultStatus() throws IOException {
         ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
 
         MinecraftOutputStream out = new MinecraftOutputStream(byteOut);
 
         out.writeVarInt(0);
 
-        StringBuilder json = new StringBuilder();
-
-        json.append("{");
-
-        json.append("\"version\": {");
-        json.append("\"name\": \"");
-        json.append(defaultVersion);
-        json.append("\",");
-
-        json.append("\"protocol\": ");
-        json.append(defaultProtocol);
-        json.append("},");
-
-        json.append("\"players\": {");
-        json.append("\"max\": 0,");
-        json.append("\"online\": 0");
-        json.append("},");
-
-        json.append("\"description\": ");
-        json.append(defaultDescription);
-
-        json.append("}");
-
-        out.writeString(json.toString());
+        out.writeString("{" + "\"version\": {" + "\"name\": \"" + defaultVersion + "\"," + "\"protocol\": " + defaultProtocol + "}," + "\"players\": {" + "\"max\": 0," + "\"online\": 0" + "}," + "\"description\": " + defaultDescription + "}");
 
         byte[] bytes = byteOut.toByteArray();
 
         return defaultStatus = bytes;
-
     }
 }
